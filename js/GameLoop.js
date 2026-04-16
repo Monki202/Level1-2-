031b08398e99dbd3d7b862ffbbf82b2a23e218ea
Monki202 <90990275+Monki202@users.noreply.github.com>
2026-04-16 13:50:58 -0400

    level1.1

---
+++ js/GameLoop.js
@@ -0,0 +1,22 @@
+var canvas, context, player, timer, interval = 1000/60
+
+canvas = document.getElementById("canvas")
+context = canvas.getContext("2d")
+
+player = new GameObject(canvas.width/2, canvas.height/2, 100, 50)
+player.color = "green"
+//player.vx = -5;
+//player.vy = 5;
+
+
+timer = setInterval(animate, interval);
+
+function animate()
+{
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    player.drawCircle();
+    
+
+}
+
